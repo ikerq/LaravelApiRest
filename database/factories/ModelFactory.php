e37a9cff0bcabf631c4e4c34723f2250bc009c95
Iker Quiros <iquiros@rayensalud.com>
2017-12-21 18:25:23 -0300

    Creando el Factory para Product

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,6 +13,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\Category;
+use App\Product;
 use App\User;
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
@@ -30,10 +31,20 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 });
 
 $factory->define(Category::class, function (Faker\Generator $faker) {
-    static $password;
-
     return [
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::PRODUCTO_DISPONIBLE, Product::PRODUCTO_NO_DISPONIBLE]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+        //'seller_id' => User::inRandomOrder()->first()->id,
+        'seller_id' => User::all()->random->id,
     ];
 });
