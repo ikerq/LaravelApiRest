232703608a6ac8b21feb2c1197dbad8b6aafca91
Iker Quiros <iquiros@rayensalud.com>
2017-12-27 18:35:40 -0300

    Removiendo un usuario con el Metodo Destroy de UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -108,6 +108,8 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+        return response()->json(['data' => $user], 200);
     }
 }
