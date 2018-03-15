ee2f6c35388a3a5a980c4d538a34dd3834723648
Iker Quiros <iquiros@rayensalud.com>
2018-03-15 17:23:36 -0300

    Preparando todos los modelos para usar "Soft Deleting"

--- app/Transaction.php
+++ app/Transaction.php
@@ -5,15 +5,18 @@ use App\Buyer;
 use App\Product;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
     protected $fillable = [
         'quantiy',
         'buyer_id',
         'product_id',
     ];
-
     public function buyer()
     {
         return $this->belongsTo(Buyer::class);
