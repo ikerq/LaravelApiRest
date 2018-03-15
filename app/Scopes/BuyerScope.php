197a33d47bf301c16faa9811a635dd0617c0bd42
Iker Quiros <iquiros@rayensalud.com>
2018-03-15 12:45:10 -0300

    Resolviendo la Inyeccion de Buyer Usando "Global Scopes"

---
+++ app/Scopes/BuyerScope.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iquiros
+ * Date: 15-03-2018
+ * Time: 12:13
+ */
+
+namespace App\Scopes;
+
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class BuyerScope implements Scope
+{
+
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder $builder
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->has('transactions');
+    }
+}
